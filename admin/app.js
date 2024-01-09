fc06f46871fd09c05c260140aa4eb53e856e69b4
GabrielRalph <gltralph@gmail.com>
2024-01-09 21:43:16 +1100

    introducing some structure

--- admin/app.js
+++ admin/app.js
@@ -1,24 +1,8 @@
 const inputs = document.querySelectorAll(".contact-input");
-
 inputs.forEach(ipt => {
-    ipt.addEventListener("focus", () => {
-        ipt.parentNode.classList.add("focus");
-        ipt.parentNode.classList.add("not-empty");
-    });
-    ipt.addEventListener("blur", () => {
-        if(ipt.value == "") {
-            ipt.parentNode.classList.remove("not-empty"); 
-        };
-        ipt.parentNode.classList.remove("focus");
-    });
+    
 });
 
-// function clearForm() {
-//     document.getElementById('meeting-description')[0].value = '';
-//     document.getElementById('when')[0].value = '';
-//     document.getElementById('finish')[0].value = '';
-//     document.getElementById('timezone')[0].value = '';
-// }
 
 document.querySelector("[name='end-time']").addEventListener("change", myFunction);
 const timeStamp = document.getElementById('time-stamp')
@@ -27,22 +11,8 @@ const timeStamp = document.getElementById('time-stamp')
 
 function myFunction() {
 
-  //value start
-  var start = Date.parse($("[name=start-time]").val()); //get timestamp
-
-  //value end
-    var end = Date.parse($("[name=end-time]").val()); //get timestamp
-
-  totalHours = NaN;
-  if (start < end) {
-    totalHours = Math.floor((end - start) / 1000 / 60); //milliseconds: /1000 / 60 / 60
-  }
-  $("#total-hours").val(totalHours);
-  timeStamp.classList.add("not-empty")
+  
 }
 
-// const changeState = () => {
-//     let fas = document.getElementById('fas');
-//     fas.classList.toggle("fa-angle-up");
-// }
+
 
---
+++ meeting-scheduler.js
@@ -0,0 +1,91 @@
+import {SvgPlus} from "./SvgPlus/4.js"
+import { templates } from "./template.js"
+import {} from "./input-plus.js"
+
+class MeetingScheduler extends SvgPlus {
+    onconnect(){
+        this.innerHTML = templates["meeting-scheduler"];
+        const save = this.querySelector("button[name = 'save'");
+        const cancel = this.querySelector("button[name = 'cancel']");
+
+        cancel.addEventListener("click", () => {
+            const event = new Event("close");
+            this.dispatchEvent(event);
+            this.value = "";
+        });
+
+        save.addEventListener("click", () => {
+            let meeting = this.value;
+            console.log(meeting);
+            if (meeting == null) {
+                alert('please finish meeting')
+            } else {
+                const event = new Event("save");
+                event.data = meeting;
+                this.dispatchEvent(event);
+                this.classList.remove("open");
+            }
+        });
+
+        this.getInput("end-time").addEventListener("change", () => this.computeTime())
+
+    }
+
+    computeTime(){
+        var start = Date.parse(this.getInputValue("start-time")); //get timestamp
+        var end = Date.parse(this.getInputValue("end-time")); //get timestamp
+        let totalHours = NaN;
+        if (start < end) {
+            totalHours = Math.floor((end - start) / 1000 / 60); //milliseconds: /1000 / 60 / 60
+        }
+        this.setInputValue("time-stamp", totalHours);
+    }
+
+    setInputValue(name, value) {
+        let input = this.getInput(name);
+        if (input) input.value = value;
+    }
+
+    getInputValue(name) {
+        let value = "";
+        let input = this.getInput(name);
+        if (input) value = input.value;
+        return value;
+    }
+    
+    getInput(name) {
+        let input = null
+        for (let i of this.inputs) {
+            if (i.name == name) {
+                input = i;
+            }
+        }
+        return input;
+    }
+
+    get inputs(){
+        return this.querySelectorAll("input-plus");
+    }
+
+    set value(value) {
+        if (typeof value !== "object" || value == null) value = {}
+        for(let input of this.inputs){
+            let name = input.name
+            let input_value = "";
+            if (name in value) input_value = value[name];
+            input.value = input_value;
+        } 
+    }
+    
+    get value() {
+        let value = {}
+        for(let input of this.inputs){
+            value[input.name] = input.value
+            if (input.value.length == 0 && input.required)
+                return null;
+        }
+        return value;
+    }
+}
+
+SvgPlus.defineHTMLElement(MeetingScheduler);
