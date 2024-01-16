81ede584c34c0c321ed26f6a10ffbb943a062298
GabrielRalph <gltralph@gmail.com>
2024-01-16 15:13:05 +1100

    firebase login

---
+++ Firebase/firebase-config.js
@@ -0,0 +1,17 @@
+const firebaseConfigj = {
+    apiKey: "AIzaSyDOhP-lTY1EC8XuHJUnxkWO5Tw8lolaV3s",
+    authDomain: "login-with-firebase-data-5e14d.firebaseapp.com",
+    projectId: "login-with-firebase-data-5e14d",
+    storageBucket: "login-with-firebase-data-5e14d.appspot.com",
+    messagingSenderId: "356416145411",
+    appId: "1:356416145411:web:b0903c1e4ce0c40bc06636"
+};
+export const firebaseConfig = {
+    apiKey: "AIzaSyChiEAP1Rp1BDNFn7BQ8d0oGR65N3rXQkE",
+    authDomain: "eyesee-d0a42.firebaseapp.com",
+    databaseURL: "https://eyesee-d0a42-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "eyesee-d0a42",
+    storageBucket: "eyesee-d0a42.appspot.com",
+    messagingSenderId: "56834287411",
+    appId: "1:56834287411:web:999340ed2fd5165fa68046"
+  };
---
+++ Firebase/firebase.js
@@ -0,0 +1,213 @@
+import { firebaseConfig } from "./firebase-config.js"
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js'
+import { signOut, getAuth, signInWithRedirect, GoogleAuthProvider, onAuthStateChanged, sendEmailVerification, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js'
+import { getDatabase, child, push, ref as _ref, get, onValue, onChildAdded, onChildChanged, onChildRemoved, set, off } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js'
+
+let initialised = false;
+let userInitialised = false;
+let App = null;
+let Database = null;
+let Auth = null;
+let User = null;
+let StateListeners = [];
+
+// Generates a random key to use as the device's unique identifier DUID.
+function makeRandomKey() {
+    return (Math.round(Math.random() * 100000)).toString(32) + Math.round(performance.now() * 1000).toString(32) + (Math.round(Math.random() * 100000)).toString(32);
+}
+
+/* If a DUID already exists in local storage retreive that key otherwise generate a new key 
+   and store in local storage. */
+let DUID = localStorage.getItem('duid');
+if (DUID == null) {
+    DUID = makeRandomKey();
+    localStorage.setItem('duid', DUID);
+}
+
+/* If the user has changed updates the new user and calls all listeners with the new user data.
+   If a listener returns the string "remove" then the listener will be removed */
+async function authChangeHandler(user) {
+    // If the user has changed to or from null OR a new user has logged in
+    if (((user == null) != (User == null)) || (user != null && User != null && user.uid != user.uid) || !userInitialised) {
+        // Update the user object
+        User = user;
+        if (User != null) updateUserData();
+        let newListeners = [];
+        // Call listeners with the new user
+        for (let obj of StateListeners) {
+            if (obj instanceof Function) {
+                if (obj(user) != "remove") newListeners.push(obj);
+            } else if (typeof obj === 'object' && obj !== null) {
+                if (obj.onauthchange instanceof Function) {
+                    if (obj.onauthchange(user) != "remove") newListeners.push(obj);
+                }
+            }
+        }
+        StateListeners = newListeners;
+    }
+}
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ PUBLIC FUNCTIONS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+/*  Initialize firebase, initializes the firebase app with the given configuration
+    after initializing wait for an auth state change and return */
+export async function initialise(config = firebaseConfig) {
+    if (initialised) return;
+    initialised = true;
+    App = initializeApp(config);
+    Database = getDatabase(App);
+    Auth = getAuth();
+    return new Promise((resolve, reject) => {
+        onAuthStateChanged(Auth, async (userData) => {
+            console.log("auth state change: user data", userData);
+            if (!userInitialised) {
+                resolve();
+            }
+            authChangeHandler(userData);
+            userInitialised = true;
+        });
+    });
+}
+
+//  Add an auth state change listener
+export function addAuthChangeListener(obj) {
+    StateListeners.push(obj);
+}
+
+// Get user uid, if none exists then the DUID is returned instead
+export function getUID() {
+    let uid = DUID;
+    if (User != null && typeof User !== "string") {
+        uid = User.uid;
+    }
+    console.log(User);
+    return uid;
+}
+
+// Get user data object
+export function getUser() { return User; }
+
+// Get App object
+export function getApp() { return App; }
+
+// Get Database object
+export function getDB() { return Database; }
+
+// Get Ref using database
+export function ref(path) { return _ref(Database, path); }
+
+
+async function updateUserData() {
+    if (await getUserData("info") == null) {
+        console.log(User);
+        // await setUserInfo({
+        //     displayName: User.displayName,
+        // })
+    }
+}
+async function getUserData(path) {
+    if (User) {
+        path = typeof path === "string" ? "/" + path : "";
+        let infoRef = ref('users/' + User.uid + path);
+        let sc = await get(infoRef, info);
+        userData = sc.val();
+    }
+    return userData;
+}
+async function setUserInfo(info) {
+    if (User) {
+        let infoRef = ref('users/' + User.uid + '/info');
+        await set(infoRef, info);
+    }
+}
+
+class LoginError extends Error {
+    constructor(error) {
+        let erroCode = error;
+        if (error.code) errorCode = error.code;
+        // Display the error message
+        let message = "";
+        switch (errorCode) {
+            case "auth/invalid-login-credentials":
+                message = "wrong email and/or password";
+                break;
+
+            case "auth/invalid-email":
+                message = "wrong email";
+                break;
+
+            case "auth/wrong-password":
+                message = "wrong password";
+                break;
+
+            // TODO: Check other errors
+            default:
+                message = errorCode;
+                break;
+
+        }
+        super(message);
+    }
+}
+
+
+export async function signin(type, info) {
+    switch (type) {
+        case "email":
+            let { email, password } = info;
+            try {
+                await signInWithEmailAndPassword(Auth, email, password);
+            } catch (error) {
+                throw new LoginError(error);
+            }
+            break;
+
+        case "gmail":
+            const provider = new GoogleAuthProvider();
+            console.log(Auth);
+            signInWithRedirect(Auth, provider);
+            break;
+
+        case "facebook":
+            throw new LoginError("Facebook has not yet been setup.");
+    }
+}
+
+export async function signup(type, info) {
+    switch (type) {
+        case "email":
+            let { email, password } = info;
+            delete info.email;
+            delete info.password;
+            try {
+                // Register user
+                await createUserWithEmailAndPassword(Auth, email, password);
+                let user = Auth.currentUser;
+
+                // Set user info
+                setUserInfo(info);
+
+                // Send email verification
+                const actionCodeSettings = {
+                    url: window.location.origin,
+                    handleCodeInApp: true
+                };
+                await sendEmailVerification(user, actionCodeSettings);
+            } catch (error) {
+                throw new LoginError(error);
+            }
+            break;
+
+        case "gmail":
+            const provider = new GoogleAuthProvider();
+            signInWithRedirect(Auth, provider);
+            break;
+
+        case "facebook":
+            throw new LoginError("Facebook has not yet been setup.");
+    }
+}
+
+export function signout(){signOut(Auth)}
+
+export {child, get, push, set, onChildAdded, onValue }
