129c1822c44dde04dc28de875154c9e013d0330e
GabrielRalph <gltralph@gmail.com>
2025-01-13 22:30:20 +1100

    change loader

--- app.js
+++ app.js
@@ -13,9 +13,9 @@ let appView = new AppView();
 let Type = null;
 function showScreen(type) {
     if (Type == null) {
-        let splash = document.querySelector("splash-screen");
-        splash.toggleAttribute("fade", true);
-        setTimeout(() => splash.remove(), 300);
+        setTimeout(() => {
+            document.querySelector("squidly-loader").hide(0.5);
+        },500);
     }
     if (type !== Type) {
         Type = type;
@@ -24,23 +24,6 @@ function showScreen(type) {
         document.body.appendChild(pages[type]);
     }
 }
-
-// loginPage.addEventListener("signin", async (e) => {
-//     try {
-//         await F.signin("email", e.data);
-//     } catch (e) {
-//         loginPage.signinError = e;
-//     }
-// });
-
-// loginPage.addEventListener("signup", async (e) => {
-//     try {
-//         await F.signup("email", e.data);
-//     } catch (err) {
-//         loginPage.signupError = err;
-//     }
-// })
-
 
 // Sometimes user may already be authenticated before siging in to the app
 let noUser = true;
@@ -63,14 +46,7 @@ F.addDataListener( (value) => {
     showScreen("appView")
 })
 
-window.F = F;
-
-
-
-
-// console.log("here");
 await F.initialise();
-// console.log("here2");
 
 
 
