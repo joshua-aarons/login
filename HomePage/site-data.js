dc056be92545c6750871dd6fad51e99e4a305ffc
GabrielRalph <gltralph@gmail.com>
2025-07-20 17:42:02 +1000

    removed testis

--- HomePage/site-data.js
+++ HomePage/site-data.js
@@ -28,30 +28,30 @@ export const Testimonials = [
         name: "Jeffrey Ding",
         title: "Mentor"
     },
-    {
-        img: "./images/people/sarah.jpg",
-        text: "Squidly is the perfect research and therapy intervention platform I can use virtually with clients across Australia.",
-        name: "Dr Sarah Reedman",
-        title: "Postdoc Research Fellow"
-    },
-    {
-        img: "./images/people/lauren.png",
-        text: "I went from never hearing about Squidly to many SPs telling me it's their new favourite tool. Seemingly overnight!",
-        name: "Lauren Curtis",
-        title: "Clinical Educator"
-    },
+    // {
+    //     img: "./images/people/sarah.jpg",
+    //     text: "Squidly is the perfect research and therapy intervention platform I can use virtually with clients across Australia.",
+    //     name: "Dr Sarah Reedman",
+    //     title: "Postdoc Research Fellow"
+    // },
+    // {
+    //     img: "./images/people/lauren.png",
+    //     text: "I went from never hearing about Squidly to many SPs telling me it's their new favourite tool. Seemingly overnight!",
+    //     name: "Lauren Curtis",
+    //     title: "Clinical Educator"
+    // },
     {
         img: "./images/people/fuad.png",
         text: "Squidly is goated for real. I feel included, and connected.",
         name: "Fuad Ferdous",
         title: "Student"
     },
-    {
-        img: "./images/people/avril.png",
-        text: "As an eye-gaze user, this gives me back my autonomy and privacy.",
-        name: "Avril P",
-        title: "AAC users"
-    },
+    // {
+    //     img: "./images/people/avril.png",
+    //     text: "As an eye-gaze user, this gives me back my autonomy and privacy.",
+    //     name: "Avril P",
+    //     title: "AAC users"
+    // },
     {
         img: "./images/people/gabrielle.jpg",
         text: "Started using Squidly yesterday and I'm blown away. Engaging, accessible, and inclusive for all my students.",
