c5507b35330c20eb7e1f2f077afde9d870ab5beb
GabrielRalph <gltralph@gmail.com>
2026-05-25 23:01:31 +1000

    phistory updates

--- Console/templates/MainPages/clients-page/profile-history.js
+++ Console/templates/MainPages/clients-page/profile-history.js
@@ -1,17 +1,104 @@
 import { SvgPlus } from "../../../../SvgPlus/4.js";
+const Option2Text = {
+  "colour-1": "Black/White",
+  "colour-2": "White/Black",
+  "colour-3": "Black/Yellow",
+  "colour-4": "Black/Green",
+  "colour-5": "Blue/Yellow",
+  "v-side": "Side",
+  "v-top": "Top",
+  "arrow": "Arrow",
+  "guide": "Guide",
+  "circle": "Focus Ring",
+};
+
+
+function camelCaseToText(str) {
+  str = str.replace(/([a-z])([A-Z])/g, '$1 $2'); // Add space before capital letters
+  return str.charAt(0).toUpperCase() + str.slice(1); // Capitalize first letter
+}
+function snakeCaseToText(str) {
+  str = str.split(/[_-]/g)
+    .map(s => s.charAt(0).toUpperCase() + s.slice(1))
+    .join(" ");
+  return str;
+}
+function formatSettingValue(value) {
+  if (typeof value === "boolean") {
+    return value ? "On" : "Off";
+  } else if (value in Option2Text) {
+    return Option2Text[value];
+  } else if (typeof value === "number") {
+    return value.toFixed(1);
+  } else {
+    return value;
+  }
+}
+
+function formatSettingKey(key) {
+  key = key.replace(/^(participant|host)\//, "");
+  key = key.split("/").map(camelCaseToText).join(" / ");
+  return key;
+}
+
+/**
+ * @param {number} minutes - The duration in minutes to format.
+ */
+function formatMinutes(minutes) {
+  let res = ""
+  if (typeof minutes === "boolean") {
+    res = minutes ? "On" : "Off";
+  } else if (minutes < 1) {
+    res =  `${Math.round(minutes * 60)}s`;
+  } else if (minutes < 60) {
+    const minute1dp = minutes.toFixed(1);
+    if (minute1dp.endsWith(".0")) {
+      res = `${Math.round(minutes)}m`;
+    } else {
+      res = `${minute1dp}m`;
+    }
+  } else {
+    const hours = Math.floor(minutes / 60);
+    const remainingMinutes = Math.round(minutes % 60);
+    if (remainingMinutes === 0) {
+      res = `${hours}h`;
+    } else {
+      res = `${hours}h ${remainingMinutes}m`;
+    }
+  }
+  return res;
+}
+
+class SHistorySection extends SvgPlus {
+  constructor(title) {
+    super("div");
+    this.class = "section";
+    this.createChild("div", {class: "section-label", innerHTML: title});
+    this._content = this.createChild("div", {class: "section-content"});
+    this.dash = this._content.createChild("span", {content: "-"});
+  }
+  get content() {
+    this.dash.remove();
+    return this._content;
+  }
+}
+class SHistoryRow extends SvgPlus {
+  constructor(label, value) {
+    super("div");
+    this.class = "row-data";
+    this.createChild("span", {innerHTML: label});
+    this.createChild("span", {innerHTML: value, style: {"font-weight": 600}});
+  }
+  
+}
+
 
 
 class SessionCard extends SvgPlus {
   constructor(sessionData) {
     super("div");
     this.class = "session-card";
-    this.optionToText = {
-      "colour-1": "Black/White",
-      "colour-2": "White/Black",
-      "colour-3": "Black/Yellow",
-      "colour-4": "Black/Green",
-      "colour-5": "Blue/Yellow",
-    };
+    
 
     const d = new Date(sessionData.metadata.time);
     let date = d.toLocaleDateString(undefined, {month: "short", day: "2-digit", year: "numeric"});
@@ -26,75 +113,51 @@ class SessionCard extends SvgPlus {
     timeInfo.createChild("span", {innerHTML: time});
 
     this.container = this.session.createChild("div", {class: "session-container"});
-    this.container.createChild("span", {innerHTML: sessionData.metadata.duration + " min"});
+    this.container.createChild("span", {innerHTML: formatMinutes(sessionData.metadata.duration)});
     this.arrow = this.container.createChild("span", {name: "arrow", innerHTML: "▾", style: {"font-size": "1.6em", "margin-left": "auto"}});
 
     // Expandable section
     this.panel = this.createChild("div", {class: "panel"});
     this.session.addEventListener("click", () => this.onPanelClicked());
 
+    
     // Calibration
-    const calibrationSection = this.panel.createChild("div", {class: "section"});
-    calibrationSection.createChild("div", {class: "section-label", innerHTML: "EYE GAZE SCORES"});
-    let calibrationContent = calibrationSection.createChild("div", {class: "section-content"});
-    if (!sessionData.calibrationScores || sessionData.calibrationScores.length === 0) {
-      calibrationContent.createChild("span", {innerHTML: "-"})
-    } else {
-      sessionData.calibrationScores.forEach((score, i) => {
-        let row = calibrationContent.createChild("div", {class: "row-data"});
-        row.createChild("span", {innerHTML: `Calibration ${i + 1}`});
-        row.createChild("span", {innerHTML: `${score}%`, style: {"font-weight": 600}});
+    const calibrationSection = this.panel.createChild(SHistorySection, {}, "EYE GAZE SCORES");
+    console.log(sessionData); 
+    let calibrationScores = (sessionData.calibrationScores || []).filter(([score, isHost]) => !isHost);
+    if (calibrationScores.length > 0) {
+      calibrationScores.forEach(([score], i) => {
+        calibrationSection.content.createChild(SHistoryRow, {}, `Calibration ${i + 1}`, `${score}%`);
       });
-    }
+    } 
 
     // Settings changes
-    const settingsSection = this.panel.createChild("div", {class: "section"});
-    settingsSection.createChild("div", {class: "section-label", innerHTML: "SETTINGS"});
-    let settingsContent = settingsSection.createChild("div", {class: "section-content"});
-    if (!sessionData.settings || Object.keys(sessionData.settings).length === 0) {
-      settingsContent.createChild("span", {innerHTML: "-"})
-    } else {
-      let hasChanges = false;
-      for (const [settingPath, setting] of sessionData.settings) {
-        if (setting.oldValue === setting.newValue) continue;
-        hasChanges = true;
-        let row = settingsContent.createChild("div", {class: "row-data"});
-        const displaySetting = settingPath.replace(/^(participant|host)\//, "");
-        row.createChild("span", {innerHTML: `${displaySetting.charAt(0).toUpperCase() + displaySetting.slice(1)}`});
-        let settingDiv = row.createChild("div");
-        let oldValue = typeof setting.oldValue  === "number" ? Math.round(setting.oldValue * 10) / 10 : setting.oldValue;
-        let newValue = typeof setting.newValue  === "number" ? Math.round(setting.newValue * 10) / 10 : setting.newValue;
-        if (this.optionToText[oldValue] || this.optionToText[newValue]) {
-          oldValue = this.optionToText[oldValue];
-          newValue = this.optionToText[newValue];
-        }
-        settingDiv.createChild("span", {innerHTML: `${oldValue} → `, style: {"color": "#b3b7c0"}});
-        settingDiv.createChild("span", {innerHTML: `${newValue}`, style: {"font-weight": 600}});
-      };
-      if (!hasChanges) {
-        settingsContent.createChild("span", {innerHTML: "-"});
-      }
-    }
-
-    // Access methods
-    const accessSection = this.panel.createChild("div", {class: "section"});
-    accessSection.createChild("div", {class: "section-label", innerHTML: "ACCESS"});
-    let accessContent = accessSection.createChild("div", {class: "section-content-wrap"});
-    let accessUsed = false;
-    Object.keys(sessionData.access).forEach((method) => {
-      if (sessionData.access[method]) {
-        accessUsed = true;
-        accessContent.createChild("div", {
-          class: "data-div",
-          style: {color: "#7380ec", "background-color": "#eef2ff", border: "1px solid #d8e3ff"},
-          innerHTML: method.charAt(0).toUpperCase() + method.slice(1)
+    const settingsSection = this.panel.createChild(SHistorySection, {}, "SETTINGS");
+    if (sessionData.settings) {
+      const settingsChanges = sessionData.settings.filter(([_, {oldValue, newValue}]) => oldValue !== newValue);
+      if (settingsChanges.length > 0) {
+        settingsChanges.forEach(([settingPath, {oldValue, newValue}]) => {
+          settingsSection.content.createChild(SHistoryRow, {class: "setting-change"}, 
+            formatSettingKey(settingPath), 
+            `<i>${formatSettingValue(oldValue)}</i> → ${formatSettingValue(newValue)}`
+          );
         });
       }
-    })
-    if (!accessUsed) {
-      accessContent.createChild("span", {innerHTML: "-"});
     }
+ 
 
+    // Access methods
+    const accessSection = this.panel.createChild(SHistorySection, {}, "ACCESS");
+    const usedAccessMethods = Object.entries(sessionData.access).filter(([_, time]) => time > 0);
+    if (usedAccessMethods.length > 0) {
+      Object.keys(sessionData.access).forEach((method) => {
+        accessSection.content.createChild(SHistoryRow, {}, 
+          snakeCaseToText(method), 
+          formatMinutes(sessionData.access[method])
+        );
+      })
+    }
+   
     // AAC 
     const aacSection = this.panel.createChild("div", {class: "section"});
     aacSection.createChild("div", {class: "section-label", innerHTML: "AAC"});
@@ -127,34 +190,30 @@ class SessionCard extends SvgPlus {
     }
 
     // Apps 
-    const appsSection = this.panel.createChild("div", {class: "section"});
-    appsSection.createChild("div", {class: "section-label", innerHTML: "APPS"});
-    let appsContent = appsSection.createChild("div", {class: "section-content"});
-    if (!sessionData.apps || Object.keys(sessionData.apps).length === 0) {
-      appsContent.createChild("span", {innerHTML: "-"})
-    } else {
+    const appsSection = this.panel.createChild(SHistorySection, {}, "APPS");
+    if (sessionData.apps && sessionData.apps.length > 0) {
       for (const [app, duration] of sessionData.apps) {
-        let row = appsContent.createChild("div", {class: "row-data"});
-        row.createChild("span", {innerHTML: app});
-        row.createChild("span", {innerHTML: `${duration} min`, style: {"font-weight": 600}})
+        let row = appsSection.content.createChild(SHistoryRow, {}, 
+          app, 
+          formatMinutes(duration)
+        );
       }
     }
 
 
     // Quizzess 
-    const quizzesSection = this.panel.createChild("div", {class: "section"});
-    quizzesSection.createChild("div", {class: "section-label", innerHTML: "QUIZZES"});
-    let quizzesContent = quizzesSection.createChild("div", {class: "section-content"});
-    if (!sessionData.quizzes || Object.keys(sessionData.quizzes).length === 0) {
-      quizzesContent.createChild("span", {innerHTML: "-"})
-    } else {
-      for (const [quiz, duration] of sessionData.quizzes) {
-        let row = quizzesContent.createChild("div", {class: "row-data"});
-        row.createChild("span", {innerHTML: quiz});
-        row.createChild("span", {innerHTML: `${duration} min`, style: {"font-weight": 600}})
+    const quizzesSection = this.panel.createChild(SHistorySection, {}, "QUIZZES");
+    if (sessionData.quizzes && sessionData.quizzes.length > 0) {
+      for (const [app, duration] of sessionData.quizzes) {
+        let row = quizzesSection.content.createChild(SHistoryRow, {}, 
+          app, 
+          formatMinutes(duration)
+        );
       }
     }
   }
+
+ 
 
   onPanelClicked() {
       const isOpen = this.panel.style.maxHeight && this.panel.style.maxHeight !== "0px";
