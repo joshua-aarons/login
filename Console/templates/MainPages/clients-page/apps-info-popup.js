e7e17d58b10d852b0d94ebf2c863965dfef411e0
GabrielRalph <gltralph@gmail.com>
2026-06-09 14:06:40 +1000

    overview

--- Console/templates/MainPages/clients-page/apps-info-popup.js
+++ Console/templates/MainPages/clients-page/apps-info-popup.js
@@ -1,6 +1,6 @@
 import { SvgPlus } from "../../../../SvgPlus/4.js";
 import { DiscreteHeatmap } from "../../../../Utilities/discreteHeatmap.js";
-import { formatMinutes } from "../../../../Utilities/utils.js";
+import { camelCaseToText, formatMinutes, snakeCaseToText } from "../../../../Utilities/utils.js";
 
 const mode2name = {
     "0": "Mouse",
@@ -34,10 +34,13 @@ class ProgressBar extends SvgPlus {
 }
 
 class BalanceCard extends SvgPlus {
-    constructor(val1, val2) {
+    constructor(val1, val2, title = "Host / Participant Balance", subtitle = null) {
         super("div");
         this.class = "balance card";
         this.createChild("div", {class: "title", content: "Host / Participant Balance"});
+        if (subtitle) {
+            this.createChild("div", {class: "subtitle", content: subtitle});
+        }
 
         let total = val1 + val2;
         let p1 = val1 / total * 100;
@@ -63,6 +66,41 @@ class BalanceCard extends SvgPlus {
         });
     }
 }
+
+class BarGraph extends SvgPlus {
+    constructor(entries) {
+        super("div");
+        this.class = "bar-graph";
+        let nonZero = entries.filter(e => e.value > 0);
+        let sum = nonZero.reduce((a, b) => a + b.value, 0);
+        let n = nonZero.length;
+        let legends = [];
+        let bar = this.createChild("div", {class: "bar"});
+        nonZero.forEach((e, i) => {
+            let color = e.color || `hsl(${i / n * 360}, 70%, 50%)`;
+            let segment = bar.createChild("div", {
+                styles: {
+                    background: color,
+                    width: (e.value / sum * 100) + "%",
+                }
+            })
+            legends.push([e.name, color]);
+        })
+        let legend = this.createChild("div", {class: "legend"});
+        legends.forEach(([name, color]) => {
+            let item = legend.createChild("div", {class: "legend-item"});
+            item.createChild("div", {class: "legend-color", styles: {background: color}});
+            item.createChild("span", {content: name});
+        })
+    }
+}
+
+const Key2Text = {
+    "dwell": "eye gaze",
+    "click": "mouse",
+    "switch": "switch",
+}
+
 class InteractionCard extends SvgPlus {
     constructor(interactions) {
         super("div");
@@ -74,7 +112,7 @@ class InteractionCard extends SvgPlus {
             let value = interactions[key];
             let percent = totalInteractions > 0 ? (value / totalInteractions * 100) : 0;
             let row = table.createChild("tr");
-            row.createChild("td", {class: "interaction-key", content: key});
+            row.createChild("td", {class: "interaction-key", content: Key2Text[key] || key});
             let bar = row.createChild("td", {styles: {
                 width: "100%",
             }}).createChild(ProgressBar, {}, percent, "--" + key + "-color");
@@ -146,6 +184,79 @@ class AppView extends SvgPlus {
     }
 }
 
+class OverviewView extends SvgPlus {
+    constructor(sessionData) {
+        super("div");
+        this.class = "app-view";
+
+        if (sessionData.deltas) {
+            let dc = this.createChild("div", {class: "deltas card",
+                styles: { "grid-column-end": 3, "grid-column-start": 1 }
+            });
+
+            let dd = sessionData.deltas.duration;
+            dc.createChild("div", {content: "Duraction "}).createChild("span", {
+                class: "word-tag",
+                content: (dd > 0 ? "▲" : dd < 0 ? "▼" : "") + formatMinutes(Math.abs(dd)),
+                [dd >= 0 ? "aqua-green" : "red"]: true,
+            });
+
+            let ti = sessionData.deltas.host.total + sessionData.deltas.participant.total;
+            dc.createChild("div", {content: "Interactions "}).createChild("span", {
+                class: "word-tag",
+                content: (ti > 0 ? "▲" : ti < 0 ? "▼" : "") + Math.abs(ti),
+                [ti >= 0 ? "aqua-green" : "red"]: true,
+            });
+
+            let pt = sessionData.deltas.participation;
+            dc.createChild("div", {content: "Participation "}).createChild("span", {
+                class: "word-tag",
+                content: (pt > 0 ? "▲" : pt < 0 ? "▼" : "") + Math.abs(Math.round(pt*100)) + "%",
+                [pt >= 0 ? "aqua-green" : "red"]: true,
+            });
+        }
+
+        let sb = this.createChild("div", {class: "card",
+            styles: { "grid-column-end": 3, "grid-column-start": 1 }
+        });
+        sb.createChild("div", {class: "title", content: "Session Breakdown"});
+        sb.createChild(BarGraph, {}, sessionData.appsInfo.map(([appName, appData]) => {
+            return {
+                name: camelCaseToText(snakeCaseToText(appName)) + " <b>" + formatMinutes(appData.duration) + "</b>",
+                value: appData.duration,
+            }
+        }))
+        
+
+
+        let ic = []
+        let totals = {host: 0, participant: 0};
+        for (let user of ["host", "participant"]) {
+            let totalUserInteractions = {...sessionData.totals[user]}
+            delete totalUserInteractions.total
+            let totalInteractions = sessionData.totals[user].total || 0;
+            
+            ic.push(new InteractionCard(totalUserInteractions));
+            let e = this.createChild("div", {class: "s-row"});
+            e.createChild("span",{
+                class: "user-type", 
+                content: user.toUpperCase() + " DATA"
+            });
+            e.createChild("span", {
+                class: "word-tag", 
+                [totalInteractions > 0 ? "purple" : "gray"]: true,
+                content: `${totalInteractions} interactions`}
+            );
+            totals[user] = totalInteractions;
+        }
+        ic.forEach(card => this.appendChild(card));
+
+        let balanceCard = new BalanceCard(totals.host, totals.participant, "Overall Host / Participant Balance", "Based on total interactions across all apps");
+        balanceCard.styles = { "grid-column-end": 3, "grid-column-start": 1 }
+        sb.after(balanceCard);
+    }
+}
+
 export class AppInfoPopup extends SvgPlus {
     constructor(sessionData) {
         super("div");
@@ -169,21 +280,32 @@ export class AppInfoPopup extends SvgPlus {
         let list = header.createChild("div", {class: "list"});
         let main = this.createChild("div", {class: "info-area"});
 
-        let items = apps.map(([appName, appData]) => {
-            let app = new AppView(appName, appData);
-            let item = list.createChild("div", {class: "list-item", content: appName});
+        let makeItem = (name, app, duration) => {
+            let niceName = camelCaseToText(snakeCaseToText(name));
+            let item = list.createChild("div", {class: "list-item", content: niceName});
             item.addEventListener("click", () => {
                 main.innerHTML = "";
                 main.appendChild(app);
-                maintitle.innerHTML = appName + "&nbsp;&nbsp;";
+                maintitle.innerHTML = niceName + "&nbsp;&nbsp;";
                 maintitle.createChild("div", {class: "duration-tag",
                     styles: {"font-size": "0.65em"},
-                    content: formatMinutes(appData.duration)
+                    content: formatMinutes(duration)
                 });
                 [...list.children].forEach(child =>child.toggleAttribute("selected", child === item));
             });
             return item;
+        }
+
+        let overview = new OverviewView(sessionData);
+        let item = makeItem("Overview", overview, sessionData.metadata.duration);
+
+        let items = apps.map(([appName, appData]) => {
+            let app = new AppView(appName, appData);
+            makeItem(appName, app, appData.duration);
         });
+        items.unshift(item);
+        
+       
         items[0].click();
     }
 }
--- Firebase/session-logs.js
+++ Firebase/session-logs.js
@@ -5,6 +5,57 @@ let watchers = [];
 export function watch(uid, allData, updateCallback) {
     let end = onValue(ref(`users/${uid}/profile-logs`), (snapshot) => {
         let sessionLogs = snapshot.val();
+
+        for (let pid in sessionLogs) {
+            let lastDuration = 0
+            let last = null;
+            let profileLogs = sessionLogs[pid];
+            for (let sid in profileLogs) {
+                let slogs = profileLogs[sid];
+                let duration = slogs.metadata.duration || 0;
+                let totalInteractions = {
+                    host: {click: 0, dwell: 0, switch: 0, total: 0},
+                    participant:{click: 0, dwell: 0, switch: 0, total: 0},
+                }
+                if (slogs.appsInfo) {
+                    for (let [appName, appData] of slogs.appsInfo) {
+                        for (let user in totalInteractions) {
+                            let uInts = appData[user].interactions || {};
+                            for (let intType in uInts) {
+                                totalInteractions[user][intType] += uInts[intType];
+                                totalInteractions[user].total += uInts[intType];
+                            }
+                        }
+                    }
+                }
+                let all = totalInteractions.host.total + totalInteractions.participant.total;
+                totalInteractions.participation = totalInteractions.participant.total / (all > 0 ? all : 1);
+                slogs.totals = totalInteractions;
+                if (last) {
+                    slogs.deltas = {
+                        duration: duration - lastDuration,
+                        host: {
+                            click: totalInteractions.host.click - last.host.click,
+                            dwell: totalInteractions.host.dwell - last.host.dwell,
+                            switch: totalInteractions.host.switch - last.host.switch,
+                            total: totalInteractions.host.total - last.host.total,
+                        },
+                        participant: {
+                            click: totalInteractions.participant.click - last.participant.click,
+                            dwell: totalInteractions.participant.dwell - last.participant.dwell,
+                            switch: totalInteractions.participant.switch - last.participant.switch,
+                            total: totalInteractions.participant.total - last.participant.total,
+                        },
+                        participation: totalInteractions.participation - (last.participation || 0),
+                    }
+                }
+                last = totalInteractions;
+                lastDuration = duration;
+            }
+        }
+
+        console.log("Session Logs Updated:", sessionLogs);
+
         allData.sessionLogs = sessionLogs;
         updateCallback();
     })
